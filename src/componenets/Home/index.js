cbfde050cfffeb9e1fa0cd3f4555f700123653c5
Andres Gallego <andresgllg112@gmail.com>
2024-05-17 23:09:53 -0700

    done

--- src/componenets/Home/index.js
+++ src/componenets/Home/index.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import LogoTitle from '../../assets/images/logo-s.png';
 import { Link } from 'react-router-dom';
 import AnimatedLetters from '../AnimatedLetters';
-import Logo from './Logo';
 import './index.scss';
 import Loader from 'react-loaders'
 
@@ -10,7 +9,7 @@ import Loader from 'react-loaders'
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     const nameArray = ['n', 'd', 'r', 'e', 's'];
-    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.' ]
+    const jobArray = ['S', 'o', 'f', ' t', 'w', 'a', 'r', 'e', ' ', 'E', 'n', 'g', 'i', 'n', 'e', 'e', 'r' ]
 
     useEffect(() => {
         const timeoutId = setTimeout(() => {
@@ -28,7 +27,7 @@ const Home = () => {
                     <span className={letterClass}>H</span>
                     <span className={`${letterClass} _12`}>i,</span>
                     <br />
-                    <span className={`${letterClass} _13`}>I,</span>
+                    <span className={`${letterClass} _13`}>I</span>
                     <span className={`${letterClass} _14`}>'m</span>
                     <img src={LogoTitle} alt="developer"/>
                     <AnimatedLetters letterClass={letterClass}
@@ -37,12 +36,12 @@ const Home = () => {
                     <br/>
                     <AnimatedLetters letterClass={letterClass}
                     strArray={jobArray}
-                    idx={22} />
+                    idx={19} />
                     </h1>
             
                     <Link to="/contact" className="flat-button">CONTACT ME</Link>
                 </div>
-                <Logo />
+
             </div>
         <Loader type="ball-spin-fade-loader" />
         </>
